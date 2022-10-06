3900d8d92e7e1c5338b257a43e4c7471e144bb32
dinacindi101 <114142490+cindi101@users.noreply.github.com>
2022-10-06 23:07:36 +0700

    Add files via upload

---
+++ task_2/detail-produk.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Detail Produk</title>
+    </head>
+    <style>
+        h2 {
+            color: green;
+        }
+        .box {
+                border: 1px solid;
+        }
+        .desain-layout {
+            width: 200px;
+        }
+    </style>
+    <body>
+        <h2>Detail Produk</h2>
+        <div class="desain-layout">
+            <div class="box">
+                <img width="200" src="https://i.postimg.cc/NyNCTW2K/dompet.jpg" alt="">
+                <h3>Dompet Wanita. MURAH!!!</h3>
+                <h4>Rp.50.000,-</h4>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ task_2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Produk</title>
+
+    </head>
+    <body>
+        <h1>Produk</h1>
+        <form action="detail-produk.html" method="GET">
+            <label for="nama-barang">Nama Barang :</label>
+            <input type="text" name="nama-barang"> <br>
+            <button type="submit">lihat detail</button>
+        </form>    
+    </body>
+</html>
